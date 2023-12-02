3b2486e7de32348d1b3b0310b4f438471a9f3499
Ryan Godwin <ryan.godwin524@outlook.com>
2023-12-02 11:13:27 -0600

    small changes to Day1::checkWordNumber(). extract out string sub to be initialized once.

--- AdventofCode2023Day1/Day1.cpp
+++ AdventofCode2023Day1/Day1.cpp
@@ -4,15 +4,15 @@ using namespace std;
 #include<iostream>
 #include "./headers/Day1.h"
 
-int Day1::checkWordNumber(string text, int i) {
+//i is the position in text to start at.
+int Day1::checkWordNumber(string &text, int i) {
+    string sub = "";
     if (text[i] == 'o') { //one
-        string sub;
         try { sub = text.substr(i, 3); }
         catch (exception e) {} //trycatch should only trigger if substring goes out of bounds.
         if (sub == "one") { return 1; }
     }
     else if (text[i] == 't') { //two or three
-        string sub;
         try { sub = text.substr(i, 3); }
         catch (exception e) {}
         if (sub == "two") { return 2; }
@@ -21,7 +21,6 @@ int Day1::checkWordNumber(string text, int i) {
         if (sub == "three") { return 3; }
     }
     else if (text[i] == 'f') { // four or five
-        string sub;
         try { sub = text.substr(i, 4); }
         catch (exception e) {}
         if (sub == "four") { return 4; }
@@ -30,7 +29,6 @@ int Day1::checkWordNumber(string text, int i) {
         if (sub == "five") { return 5; }
     }
     else if (text[i] == 's') { // six or seven
-        string sub;
         try { sub = text.substr(i, 3); }
         catch (exception e) {}
         if (sub == "six") { return 6; }
@@ -39,13 +37,11 @@ int Day1::checkWordNumber(string text, int i) {
         if (sub == "seven") { return 7; }
     }
     else if (text[i] == 'e') { // eight
-        string sub;
         try { sub = text.substr(i, 5); }
         catch (exception e) {}
         if (sub == "eight") { return 8; }
     }
     else if (text[i] == 'n') { // nine
-        string sub;
         try { sub = text.substr(i, 4); }
         catch (exception e) {}
         if (sub == "nine") { return 9; }
@@ -77,43 +73,33 @@ int Day1::day() {
     }
 
     //now do it again, but this time, words that are numbers count. ex) "one" == 1.
-    //one two three four five six seven eight nine
     int sum2 = 0;
 
     file.clear();   //resets the file flags
     file.seekg(0);  //sets place in file back to beginning
     int lines = 0;
+
     while (getline(file, text)) {
         lines++;
         int a = 0;
         for (int i = 0; i < text.size(); ++i) {
             if (isdigit(text[i])) {
-                a = atoi(&text[i]); //atoi grabs a whole number in a string from i->end of number,
-                //such as "12345bca" -> 12345, rather than just 1.
-                //but its fine working backwards, you'll get just 5.
-                while (a >= 10) {
-                    a /= 10;
-                }
-                a *= 10;
+                a = atoi(&text[i]);                 //atoi grabs a whole number in a string from i->end of number,
+                while (a >= 10) { a /= 10; }        //such as "12345bca" -> 12345, rather than just 1.
+                a *= 10;                            //but its fine working backwards, you'll get just 5.
                 break;
             }
             else { //check if its a wordnumber
                 int x = checkWordNumber(text, i);
-                if (x != 0) { //didnt come back with 0
-                    a = x * 10; break;
-                }
+                if (x) { a = x * 10; break; }
             }
         }
         //now work backwards to get second number.
         for (int i = text.size() - 1; i >= 0; --i) {
-            if (isdigit(text[i])) {
-                a += atoi(&text[i]); break;
-            }
+            if (isdigit(text[i])) { a += atoi(&text[i]); break; }
             else { //check if its a wordnumber
                 int x = checkWordNumber(text, i);
-                if (x) { //didnt come back with 0
-                    a += x; break;
-                }
+                if (x) { a += x; break; }
             }
         }
         sum2 += a;
--- AdventofCode2023Day1/headers/Day1.h
+++ AdventofCode2023Day1/headers/Day1.h
@@ -3,7 +3,7 @@ class Day1
 {
 private:
 	//checks if a substring in text, starting at pos i, is a number.
-	int checkWordNumber(string text, int i);
+	int checkWordNumber(string &text, int i);
 public:
 	//basically the main function of Day1
 	int day();
