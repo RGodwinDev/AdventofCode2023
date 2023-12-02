3b3e1e4a9f9ec03306aa95c907c89857c70d22b5
Ryan Godwin <ryan.godwin524@outlook.com>
2023-12-02 11:57:22 -0600

    Day2 readability pass

--- AdventofCode2023Day1/Day2.cpp
+++ AdventofCode2023Day1/Day2.cpp
@@ -6,53 +6,54 @@ using namespace std;
 #include "./headers/Day2.h"
 
 int Day2::day() {
+
 	string line;
 	fstream file("./inputs/day2input.txt");
-	int ids = 0;
+
+	int ids = 0, sum = 0;
 	int maxreds = 12, maxgreens = 13, maxblues = 14;
-	int sum = 0;
-	int lines = 0;
+
+	int lines = 0; //count the lines in the file, thats it.
+
 	while (getline(file, line)) {
 		lines++;
+
 		int reds = 0, greens = 0, blues = 0;
-		int gameid = stoi(line.substr(4,4));
+		int gameid = stoi(line.substr(4,4)); //the gameid is always 4 into the line, and up to 4 in size.
+
 		//use 2 pointers, find :, then find ; over and over.
 		int pos = find(line.begin(), line.end(), ':') - line.begin() + 1;
 		int pos2 = find(line.begin() + pos, line.end(), ';') - line.begin() + 1;
+
 		while (pos < line.size()-1) {
-			if (pos2 >= line.size()){
-				pos2 = line.size() - 1;
-			}
-			string hand = line.substr(pos+1, (pos2 - pos) - 1);
-			//got individual handfulls now.
-			
-			stringstream ss(hand);
+
+			//last hand of a line doesnt have a ;, so go back one to stay in bounds.
+			if (pos2 >= line.size()){ pos2 = line.size() - 1; } 
+
+			//get a hand, put into stringstream
+			stringstream ss(line.substr(pos+1, (pos2 - pos) - 1));
+
 			string word;
-			int wordcount = 0;
-			int n = 0;
 			while (ss >> word) {
-				if (wordcount % 2 == 0) { //check for number
-					n = stoi(word);
-				}
-				else { //check for color
-					if (word[0] == 'r') { reds = max(reds, n); }
-					else if (word[0] == 'g') { greens = max(greens, n); }
-					else if (word[0] == 'b') { blues = max(blues, n); }
-					n = 0;
-				}
-				wordcount++;
+				int n = stoi(word); //first word is a number
+				ss >> word;			//second word is a color
+				if (word[0] == 'r') { reds = max(reds, n); }
+				else if (word[0] == 'g') { greens = max(greens, n); }
+				else if (word[0] == 'b') { blues = max(blues, n); }
 			}
 
+			//move positions to next.
 			pos = pos2;
 			pos2 = find(line.begin() + pos, line.end(), ';') - line.begin() + 1;
 		}
 		 
-		if (reds <= maxreds && greens <= maxgreens && blues <= maxblues) {
-			ids += gameid;
-		}
-		int product = reds * greens * blues;
-		sum += product;
+		//if the game had under the max possible for each color, it gets counted
+		if (reds <= maxreds && greens <= maxgreens && blues <= maxblues) { ids += gameid; }
+		//sum of powerset of all games.
+		sum += reds * greens * blues;
 	}
+
+
 	std::cout << "Day 2: " << ids << " and " << sum << endl;
 	return lines;
 }
