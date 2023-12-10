7ddd523a90d65c769d7c36f9ed950cc1314c3da9
Ryan Godwin <ryan.godwin524@outlook.com>
2023-12-10 13:13:29 -0600

    comments on Day10 header

--- AdventofCode2023Day1/headers/Day10.h
+++ AdventofCode2023Day1/headers/Day10.h
@@ -14,8 +14,14 @@ private:
 
 public:
 	int day();
+
+	//counts how many steps to get to the farthest point in the pipe from S.
+	//writes down the pipe positions in the given pipePositions.
 	int part1Traverse(std::pair<int, int> startPos, std::vector<std::string>* pipeMap, std::vector<std::pair<int,int>>* pipePositions);
 
+	//counts how many nodes are enclosed by the loop.
+	//requires the loop in traverse order, and sorted order, and the map.
+	//might want a pipeMap class with the traverse methods in it.
 	int part2Traverse(std::vector<std::string>* pipeMap, std::vector<std::pair<int, int>>* pipePositions, std::vector<std::pair<int, int>>* sortedPipePos);
 
 	//searches for a pair<int,int> inside a sorted vector<pair<int,int>>
